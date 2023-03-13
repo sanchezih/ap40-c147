8f89bf574002fe2b3128eb98beeabd18be826f12
Ignacio Sanchez <sanchezih@gmail.com>
2023-03-13 20:48:12 -0300

    update ejemplo junit

--- clase06-testing-ejemplo/src/test/java/ar/edu/utn/ap40/junit/UsuarioServicioTest.java
+++ clase06-testing-ejemplo/src/test/java/ar/edu/utn/ap40/junit/UsuarioServicioTest.java
@@ -1,34 +1,58 @@
-//package ar.edu.utn.ap40.junit;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//public class UsuarioServicioTest {
-//
-//	@DisplayName("Dado un usuario que queremos crear, esperamos que cuando creamos el usuario...")
-//	@Test
-//	public void test1() {
-//
-//		UsuarioDto esperado = new UsuarioDto(1L, "Martin");
-//
-//		UsuarioServicio usuarioServicio = new UsuarioServicio();
-//		UsuarioDto resultado = usuarioServicio.crearUsuario(1L, "NombreDePrueba");
-//		// Assertions.assertEquals(esperado, resultado); // Esto usara el metodo equals
-//		Assertions.assertEquals(esperado.id, resultado.id);
-//
-//	}
-//
-//	@Test
-//	public void test2() {
-//
-//		UsuarioDto esperado = new UsuarioDto(1L, "NombreDePruebaErroneo");
-//
-//		UsuarioServicio usuarioServicio = new UsuarioServicio();
-//		final UsuarioDto resultado = usuarioServicio.crearUsuario(1L, "NombreDePrueba");
-//		Assertions.assertNotEquals(esperado, resultado);
-//		Assertions.assertNotEquals(esperado.nombre, resultado.nombre);
-//
-//	}
-//
-//}
+package ar.edu.utn.ap40.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UsuarioServicioTest {
+
+	private UsuarioServicio usuarioServicio;
+
+	@BeforeEach
+	public void setup() {
+		usuarioServicio = new UsuarioServicio();
+		usuarioServicio.crearUsuario(1L, "Erica");
+		usuarioServicio.crearUsuario(2L, "Melina");
+	}
+
+	@DisplayName("Dado un usuario que queremos crear, esperamos que cuando creamos el usuario...")
+	@Test
+	public void test1() {
+
+		UsuarioDto esperado = new UsuarioDto(3L, "Martin");
+
+		UsuarioDto resultado = usuarioServicio.crearUsuario(3L, "Martin");
+		//Assertions.assertEquals(esperado, resultado); // Esto usara el metodo equals
+		Assertions.assertEquals(esperado.id, resultado.id);
+
+	}
+
+	@Test
+	public void test2() {
+
+		UsuarioDto esperado = new UsuarioDto(1L, "NombreDePruebaErroneo");
+
+		final UsuarioDto resultado = usuarioServicio.crearUsuario(1L, "NombreDePrueba");
+		Assertions.assertNotEquals(esperado, resultado);
+		Assertions.assertNotEquals(esperado.nombre, resultado.nombre);
+
+	}
+
+	@DisplayName("Buscar un usuario inexistente y validar que sea null")
+	@Test
+	public void test3() {
+		final UsuarioDto resultado = usuarioServicio.obtenerUsuario(3L);
+		Assertions.assertEquals(null, resultado);
+	}
+
+	@DisplayName("Actualizar el nombre de un usuario existente y validar que se haya actualizado correctamente")
+	@Test
+	public void test4() {
+		UsuarioDto esperado = new UsuarioDto(1L, "Fernanda");
+		UsuarioDto actualizado = usuarioServicio.actualizarUsuario(1L, "Fernanda");
+		UsuarioDto resultado = usuarioServicio.obtenerUsuario(1L);
+		Assertions.assertEquals(esperado.getNombre(), resultado.getNombre());
+	}
+
+}
